cf5bfd4e4b8f2a9bb972ef0f4aa6a3738c1cc665
Abhiraj Singh Chauhan <chauhanabhiraj06@gmail.com>
2025-04-08 22:59:49 +0530

    feat: add google tag manager

--- app/components/mdx/FeatureCard.tsx
+++ app/components/mdx/FeatureCard.tsx
@@ -17,7 +17,7 @@ export default function FeatureCard({
   link,
 }: FeatureCardProps) {
   const CardContent = () => (
-    <div className="flex flex-col">
+    <div className="flex flex-col" id={title} >
       <div className="flex items-start gap-3">
         <div className={`${iconColor} flex-shrink-0`}>{icon}</div>
         <h3 className="text-sm font-bold m-0  ">{title}</h3>
@@ -29,7 +29,7 @@ export default function FeatureCard({
   const cardClasses = "px-4 py-4 rounded-lg border border-zinc-200 dark:border-zinc-700 hover:border-zinc-300 dark:hover:border-zinc-600 transition-all shadow-sm hover:shadow-md";
 
   return link ? (
-    <Link href={link} className="no-underline not-prose">
+    <Link href={link} className="no-underline not-prose feature-card-analytics-tracker" id={title}>
       <div className={`${cardClasses} cursor-pointer hover:transform hover:translate-y-[-2px]`}>
         <CardContent />
       </div>
