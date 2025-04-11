8d8f82d1d23a015c2293d442ecf307998df24d2f
shouryam-kumar <shouryam1508@gmail.com>
2025-04-11 10:52:44 +0530

    changed create proposal link and added note for rewards

--- app/summer-of-abstraction/ideaboard/page.tsx
+++ app/summer-of-abstraction/ideaboard/page.tsx
@@ -79,6 +79,18 @@ export default function IdeaboardPage() {
             </div>
           </div>
           
+          {/* Reward Info Box */}
+          <div className="mb-6 bg-yellow-50 dark:bg-yellow-900/20 p-3 rounded-md border border-yellow-200 dark:border-yellow-800 text-sm">
+            <p className="text-gray-800 dark:text-gray-300 flex items-start">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 flex-shrink-0 text-yellow-600 dark:text-yellow-400 mt-0.5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+              </svg>
+              <span>
+                <span className="font-semibold">Note for Rewards:</span> $OKTO represents tokens that will be airdropped to your Okto wallet at TGE. $OKTO is primarily allocated for project contributions.
+                Okto Points symbolize rewards credited pre-TGE into your Okto wallet and convert to $OKTO tradable at TGE. Okto Points are usually awarded for bounty completions. Following TGE, all project and bounty rewards will be distributed directly in $OKTO.
+              </span>
+            </p>
+          </div>
 
           {/* Ideaboard Section */}
           <IdeasLoader>
