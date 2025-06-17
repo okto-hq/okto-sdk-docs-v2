1528246f5c20cc4ac81d3ae64406be311d511e77
Saakshi Raut <saakshiraut28@gmail.com>
2025-06-17 11:27:59 +0530

    add: twitter and apple auth script

--- scripts/generate-api-reference-docs-auth.mjs
+++ scripts/generate-api-reference-docs-auth.mjs
@@ -4,7 +4,7 @@ import path from 'path';
 
 void OpenAPI.generateFiles({
   // input files
-  input: ['./public/openapi/auth/google.json' , './public/openapi/auth/email.json' , './public/openapi/auth/whatsapp.json' , './public/openapi/auth/jwt.json'],
+  input: ['./public/openapi/auth/google.json' , './public/openapi/auth/email.json' , './public/openapi/auth/whatsapp.json' , './public/openapi/auth/jwt.json', './public/openapi/auth/apple.json','./public/openapi/auth/twitter.json' ],
   output: "./content/docs/openapi/authenticate/(auth)",
   includeDescription: true,
 });
